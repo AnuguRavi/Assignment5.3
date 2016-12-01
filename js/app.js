b904352d58b616169733af3f63882cdb2dbfd0bf
Ravi Anugu <ravi.rgu09@gmail.com>
2016-12-01 23:15:53 +0530

    Add files via upload
    JavaScript Nested Objects

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var Employee = {
+    name: "Ravi Anugu",
+    age: 23,
+    salary: 10000,
+    address: {
+        city: "Hyderabad",
+        state: "Telangana",
+        pincode: 500084
+    }
+};
+
+console.log("Name: ", Employee.name);
+console.log("Age: ",Employee.age);
+console.log("Salary: ", Employee.salary);
+console.log("City: ", Employee.address.city);
+console.log("State: ", Employee.address.state);
+console.log("Pincode: ", Employee.address.pincode);
